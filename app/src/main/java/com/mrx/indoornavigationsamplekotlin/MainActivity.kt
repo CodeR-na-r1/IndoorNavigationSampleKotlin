f0727e000f9c0fd6d60ebe9b538f2afa0f0c497a
Vlad <coder.na.r1@gmail.com>
2023-03-06 21:52:54 +0800

    ready for use

--- app/src/main/java/com/mrx/indoornavigationsamplekotlin/MainActivity.kt
+++ app/src/main/java/com/mrx/indoornavigationsamplekotlin/MainActivity.kt
@@ -2,10 +2,35 @@ package com.mrx.indoornavigationsamplekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.mrx.indoorservice.api.IndoorService
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var beaconReferenceApplication: BeaconReferenceApplication
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        beaconReferenceApplication = application as BeaconReferenceApplication
+
+        val indoorService = IndoorService.getInstance(this)
+
+        /*
+        * После первого запуска необходимо дать разрешение приложению на местоположение в настройках, иначе маяки не найдет
+        */
+
+        // todo здесь ваш далее код
+
+        indoorService.BeaconsEnvironment.getRangingViewModel().observe(this) { beacons ->
+            Log.d(TAG, "Ranged: ${beacons.count()} beacons")
+        }
+
+        indoorService.BeaconsEnvironment.startRanging()
+    }
+
+    companion object {
+        const val TAG = "MyTag"
     }
 }
