8152010fe86d956610d58bbd370f49c68b7b0439
Vlad <coder.na.r1@gmail.com>
2023-03-07 20:31:05 +0800

    optimize sample

--- app/src/main/java/com/mrx/indoornavigationsamplekotlin/MainActivity.kt
+++ app/src/main/java/com/mrx/indoornavigationsamplekotlin/MainActivity.kt
@@ -4,18 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.mrx.indoorservice.api.IndoorService
+import org.altbeacon.beacon.BeaconManager
+import org.altbeacon.beacon.BeaconParser
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var beaconReferenceApplication: BeaconReferenceApplication
+    private lateinit var beaconManager: BeaconManager
+    private lateinit var indoorService: IndoorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        beaconReferenceApplication = application as BeaconReferenceApplication
+        beaconManager = BeaconManager.getInstanceForApplication(this)
+        indoorService = IndoorService.getInstance(this)
 
-        val indoorService = IndoorService.getInstance(this)
+        // Настройка для поиска маячков iBeacon
+
+        beaconManager.beaconParsers.clear()
+        beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"))
 
         /*
         * После первого запуска необходимо дать разрешение приложению на местоположение в настройках, иначе маяки не найдет
@@ -28,6 +35,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         indoorService.BeaconsEnvironment.startRanging()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        indoorService.BeaconsEnvironment.stopRanging()
     }
 
     companion object {
