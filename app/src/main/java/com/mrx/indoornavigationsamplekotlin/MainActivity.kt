d5be7c6368293af529b93962b3fb2bd97ef790bd
Vlad <coder.na.r1@gmail.com>
2023-03-16 20:46:12 +0800

    Update lib files

--- app/src/main/java/com/mrx/indoornavigationsamplekotlin/MainActivity.kt
+++ app/src/main/java/com/mrx/indoornavigationsamplekotlin/MainActivity.kt
@@ -3,6 +3,8 @@ package com.mrx.indoornavigationsamplekotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import com.mrx.indoorservice.api.IndoorService
 import org.altbeacon.beacon.BeaconManager
 import org.altbeacon.beacon.BeaconParser
@@ -12,12 +14,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var beaconManager: BeaconManager
     private lateinit var indoorService: IndoorService
 
+    lateinit var buttonStart: Button
+    lateinit var textViewBeacons: TextView
+    lateinit var textViewAzimuth: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         beaconManager = BeaconManager.getInstanceForApplication(this)
         indoorService = IndoorService.getInstance(this)
+
+        buttonStart = findViewById(R.id.btn_control)
+        textViewBeacons = findViewById(R.id.textView_beacons)
+        textViewAzimuth = findViewById(R.id.textView_azimuth)
 
         // Настройка для поиска маячков iBeacon
 
@@ -28,19 +38,29 @@ class MainActivity : AppCompatActivity() {
         * После первого запуска необходимо дать разрешение приложению на местоположение в настройках, иначе маяки не найдет
         */
 
-        // todo здесь ваш далее код
+        buttonStart.setOnClickListener {
+            // Маячки: Подписались на обновление данных и выводим на текстовое поле
+            indoorService.BeaconsEnvironment.getRangingViewModel().observe(this) { beacons ->
+                textViewBeacons.text = "Ranged: ${beacons.count()} beacons"
+            }
 
-        indoorService.BeaconsEnvironment.getRangingViewModel().observe(this) { beacons ->
-            Log.d(TAG, "Ranged: ${beacons.count()} beacons")
+            // Азимут: Подписались на обновление данных и выводим на текстовое поле
+            indoorService.AzimuthManager.getAzimuthViewModel().observe(this) {
+                textViewAzimuth.text = "Azimuth: ${it.toInt()}"
+            }
+
+            // Запустили обнаружение маячков и прослушивание сенсора
+            indoorService.BeaconsEnvironment.startRanging()
+            indoorService.AzimuthManager.startListen()
         }
-
-        indoorService.BeaconsEnvironment.startRanging()
     }
 
     override fun onPause() {
         super.onPause()
 
+        // Останавливаем обнаружение маячков и прослушивание сенсора
         indoorService.BeaconsEnvironment.stopRanging()
+        indoorService.AzimuthManager.stopListen()
     }
 
     companion object {
